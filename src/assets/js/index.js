58bcb47f85168ea554191c2a3bb4f8231863adab
JusDePoire <antonin@dutreix.net>
2024-06-13 20:11:31 +0200

    some improvements

--- src/assets/js/index.js
+++ src/assets/js/index.js
@@ -30,9 +30,9 @@ class Splash {
 
     async startAnimation() {
         let splashes = [
-            { "message": "Je... vie...", "author": "Luuxis" },
-            { "message": "Salut je suis du code.", "author": "Luuxis" },
-            { "message": "Linux n'est pas un os, mais un kernel.", "author": "Luuxis" }
+            { "message": "Cuisson de l'Euphillium...", "author": "Euphillia" },
+            { "message": "Chargement du charabia numérique", "author": "Euphillia" },
+            { "message": "La meilleure expérience survie moddé arrive...", "author": "Euphillia" }
         ];
         let splash = splashes[Math.floor(Math.random() * splashes.length)];
         this.splashMessage.textContent = splash.message;
